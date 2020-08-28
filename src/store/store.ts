e6ecc21a04926fb8dff852c3a6bec212e1b20130
xavigu <xavigu91@gmail.com>
2020-08-28 13:27:31 +0200

    dispatch function to add new todo(payload) in the array of todos

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,24 @@
 export class Store {
   private subscribers: Function[];
-  private reducers: {[key: string]: Function}
+  private reducers: { [key: string]: Function };
   // state of the application
-  private state: { [key:string]: any};
+  private state: { [key: string]: any };
 
-  constructor(reducers = {}, initialState = {}){
+  constructor(reducers = {}, initialState = {}) {
     this.state = initialState;
   }
 
   get value() {
     return this.state;
+  }
+
+  // update the todo list array
+  dispatch(action) {
+    this.state = {
+      ...this.state,
+      todos: [...this.state.todos, action.payload],
+    };
+    console.log(this.state);
   }
   // to get store value console.log(store.value);
 }
